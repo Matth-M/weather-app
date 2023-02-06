b9bb51700415d6d820f7155dd31bca1695dbaf64
Matth-M <matthieu.matsima@gmail.com>
2023-02-06 19:06:17 +0100

    Display city weather

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import "./style.css";
 const API_KEY = "1c81b446af528187f6f55d391a3a8f8c";
 
 async function getCityWeather(city) {
@@ -11,10 +12,14 @@ async function getCityWeather(city) {
 	}
 }
 
+const cityContainer = document.querySelector(".city-weather");
+const cityNameH1 = cityContainer.querySelector("h1");
+const cityWeatherDescription = cityContainer.querySelector("p");
+
 async function displayCityWeather(city) {
 	const data = await getCityWeather(city);
-	console.log(data.name);
-	console.log(data.weather[0].main);
+	cityNameH1.textContent = data.name;
+	cityWeatherDescription.textContent = data.weather[0].main;
 }
 
 displayCityWeather("Rome");

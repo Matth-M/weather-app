dc3c44dd2824a2bc89be5a27268fbd06906f0d1c
Matth-M <matthieu.matsima@gmail.com>
2023-02-06 19:29:15 +0100

    Fetch icon corresponding to the weather

--- main.js
+++ main.js
@@ -15,11 +15,26 @@ async function getCityWeather(city) {
 const cityContainer = document.querySelector(".city-weather");
 const cityNameH1 = cityContainer.querySelector("h1");
 const cityWeatherDescription = cityContainer.querySelector("p");
+const cityWeatherIcon = cityContainer.querySelector("img");
 
 async function displayCityWeather(city) {
-	const data = await getCityWeather(city);
-	cityNameH1.textContent = data.name;
-	cityWeatherDescription.textContent = data.weather[0].main;
+	try {
+		const data = await getCityWeather(city);
+		console.log(data.weather[0].icon);
+		cityNameH1.textContent = data.name;
+		cityWeatherDescription.textContent = data.weather[0].description;
+		let icon = await fetch(createIconUrl(data.weather[0].icon), {
+			mode: "cors",
+		});
+		console.log(icon);
+		cityWeatherIcon.src = icon.url;
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+function createIconUrl(code) {
+	return `https://openweathermap.org/img/wn/${code}@2x.png`;
 }
 
 displayCityWeather("Rome");
